21c7d03859a957fade9c274366bc36e159718776
SnakeSnuggles <jacobsylvester6@gmail.com>
2024-01-03 13:35:24 -0700

    added a instructions menu for those who don't know what the program is

--- spyrandomdisguisev1.1.py
+++ spyrandomdisguisev1.1.py
@@ -12,21 +12,74 @@ TODO:
 4) Make it so when the window is closed the console will also be closed
 5) Get rid of the console 
 6) Try to store the icon in the python source 
-7) Add a instruction menu with what this is used for and how to use it
+7) Add a instruction menu with what this is used for and how to use it (D)
+8) Add the ability to change color 1 and color 2
 '''
 COLOR1 = "#350A0B"
 COLOR2 = "#540D0A"
+def readr():
+    try:
+        with open("show.txt", "r") as file:
+            line = file.readline()
+            return line
+    except FileNotFoundError:
+        with open("show.txt","w") as file:
+            file.write("")
+def readw(what_to_write) -> None:
+    with open("show.txt", "w") as file:
+        file.write(what_to_write)
+
 def window_part():
-    global to_pick_from
+    global to_pick_from 
+    should_show = readr()
+
+    if should_show != 'true':
+        instructions = tk.Tk()
+        instructions.title("Spy disguise")
+        instructions.geometry("400x200")
+        instructions.resizable(False, False)
+        instructions.config(bg=COLOR1)
+        p1 = tk.PhotoImage(file = '350px-Spytaunt3.png')
+        instructions.iconphoto(False, p1)
+
+        def end_instructions():
+            instructions.destroy()
+        def end_and_never_show():
+            readw("true")
+            instructions.destroy()
+
+        text_to_add = '''
+        To use: 
+        1) Click on the classes you want to be choosen 
+        2) Press 4 and let the program do the rest
+        Used for:
+        the spy in tf2, this allows for random choosing of spy disguises'''
+        title = tk.Label(instructions,text="Snake's Spy Disguises",bg=COLOR1,fg='white')
+        content = tk.Label(instructions,text=text_to_add,bg=COLOR1,fg='white')
+
+        continuebut = tk.Button(instructions,text="Continue",command=end_instructions)
+        continuebutnev = tk.Button(instructions,text="Continue and never show again",command=end_and_never_show)
+        title.pack()
+        title.config(font=("Arial", 12))
+        content.pack()
+        content.config(font=("Arial", 10))
+        continuebut.pack(pady=5)
+        continuebutnev.pack(pady=5)
+        continuebut.config(background=COLOR2,fg='white')
+        continuebutnev.config(background=COLOR2,fg='white')    
+        instructions.mainloop()
+
+
     def class_caller(class_var,button:str):
         ischecker = class_var.get() == 1
         if ischecker:
             to_pick_from.append(button)  
         else:
             to_pick_from.remove(button)
+
     window = tk.Tk()
     window.title("Spy disguise")
-    window.geometry("190x240")
+    window.geometry("190x260")
     window.resizable(False, False)
     window.config(bg=COLOR1)
     p1 = tk.PhotoImage(file = '350px-Spytaunt3.png')
